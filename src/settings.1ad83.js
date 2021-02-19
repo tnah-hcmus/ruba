516b36dfd322778a4534e51c1f4d8af88a198526
tnah-hcmus <tnah.work@gmail.com>
2021-02-19 16:03:11 +0700

    Games

---
+++ src/settings.1ad83.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/batDau.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"fbb78",main:"16b2f"}};
